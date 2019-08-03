d557ed436a4e66155e1bf937915eb452646b00d8
probir270 <probirsingha270@gmail.com>
2019-08-03 13:36:06 +0530

    Scripts

---
+++ Game Jam GMTK 2019/Assets/Scripts/Movements.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    public float speed = 2;
+    public GameObject mainCamera;
+    private void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    void Update()
+    {
+        float hori = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float verti = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(hori, verti, 0);
+    }
+}
