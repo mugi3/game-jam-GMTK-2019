0770bf3a610b583d52fac3f1a67e5d871e82b96a
probir270 <probirsingha270@gmail.com>
2019-08-03 13:47:13 +0530

    Dekh ab

---
+++ Game Jam GMTK 2019/Assets/Scripts/Clues_SO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="Clue",menuName ="Add New Info")]
+[System.Serializable]
+public class Clues_SO : ScriptableObject
+{
+    [TextArea(3, 10)]
+    public string dialogue;
+    
+}
